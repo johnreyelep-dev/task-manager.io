7ed77ab301b18c5f8590bdef3588d87ac3e22eef
John Rey <johnreyelep02@gmail.com>
2026-06-30 21:56:49 +0800

    Improve UI design

--- task-manager/src/pages/Sidebar.jsx
+++ task-manager/src/pages/Sidebar.jsx
@@ -23,10 +23,10 @@ export default function Sidebar({activeTab, setActiveTab}) {
                     items.map((item, index) => (
                         <li key={index}
                             onClick={() => setActiveTab(item)}
-                            className={`group relative flex text-xl git  py-2 px-5 rounded-md cursor-pointer transition-all duration-200 linear overflow-hidden
-                            ${activeTab === item ? 'text-white translate-x-5' : ''}`}>
-                            <div className={`absolute left-0 top-0 w-full h-full -z-10 transition-all duration-100 ease-linear ${activeTab === item ? "bg-secondary" :"group-hover:bg-secondary"} `}></div>
-                            <span className='relative z-10 group-hover:text-white' >{item}</span>
+                            className={`relative group flex items-center justify-center text-xl git  py-2 px-5 rounded-md cursor-pointer transition-all duration-200 linear overflow-hidden
+                             ${activeTab === item ? 'text-white translate-x-2' : ''}`}>
+                            <div className={`absolute w-full h-10 -z-10 ${activeTab === item ? "bg-secondary" : "group-hover:shadow-[0_2px_0px_rgba(139,92,246,.80)] group-hover:bg-card"}`}></div>
+                            <span className='relative z-10' >{item}</span>
                         </li>
                     ))
                 }
