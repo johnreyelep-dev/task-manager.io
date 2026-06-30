849e81e6396e2290d1efc95cdf913fffc5f0af67
John Rey <johnreyelep02@gmail.com>
2026-06-30 21:57:27 +0800

    Remove one component

--- task-manager/src/pages/Header.jsx
+++ task-manager/src/pages/Header.jsx
@@ -6,11 +6,11 @@ export default function Header(){
 
     const {theme, toggleTheme} = useTheme();
     return (
-        <div className='w-full h-18 flex justify-between items-center'>
-            <h1 className='text-2xl  font-semibold'>Johnrey</h1>
-            <form>
-                <input type='search' placeholder='Search' className='w-100 h-10 text-primary rounded-2xl px-2 outline-none border border-secondary' />
-            </form>
+        <div className='w-full h-18 flex justify-between items-center mb-3'>
+            <div className='flex items-center justify-center gap-2 '>
+                <div className='w-10 h-10 rounded-full border border-secondary'></div>
+                <h1 className='text-2xl  font-semibold'>Johnrey</h1>
+            </div>
             <div className='w-20 h-10 flex justify-between items-center gap-3 '>
                 <button className='cursor-pointer'>
                     <FontAwesomeIcon icon={faBell} style={{color: "#6B73FF",}} className='text-2xl' />
