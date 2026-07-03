b88de43cf779714fe3ada1a797f46e9f5117725b
John Rey <johnreyelep02@gmail.com>
2026-07-03 22:10:08 +0800

    Adding TaskProvider

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -2,21 +2,26 @@ import {useState} from "react";
 import Header from './pages/Header.jsx';
 import Sidebar from './pages/Sidebar.jsx';
 import Dashboard from './component/Dashboard.jsx';
+import Task from './pages/Task.jsx';
+import {TaskProvider} from "./hooks/useTask.jsx";
 import './index.css'
 
 function App() {
     const [activeTab, setActiveTab] = useState("Dashboard");
 
   return (
-    <div className='flex w-full h-screen'>
-        <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div className='w-[65%] px-5'>
-          <header>
-              <Header />
-          </header>
-          {activeTab === "Dashboard" && <Dashboard />}
-      </div>
-    </div>
+    <TaskProvider >
+        <div className='flex w-full h-screen'>
+            <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+            <div className='w-[65%] px-5'>
+                <header>
+                    <Header />
+                </header>
+                {activeTab === "Dashboard" && <Dashboard />}
+                {activeTab === "Task" && <Task />}
+            </div>
+        </div>
+    </TaskProvider>
   )
 }
 
